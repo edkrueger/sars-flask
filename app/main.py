9b57c3086d6cd08665cf849751408bdba1de1db4
Edward Krueger <edkrueger@gmail.com>
2020-03-19 14:27:08 -0500

    Automatically handle sqlalchemy sessions.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from typing import List
 
-from flask import Flask, jsonify, url_for
+from flask import Flask, _app_ctx_stack, jsonify, url_for
 from flask_cors import CORS
+from sqlalchemy.orm import scoped_session
 
 from . import models
 from .database import SessionLocal, engine
@@ -10,6 +11,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = Flask(__name__)
 CORS(app)
+app.session = scoped_session(SessionLocal, scopefunc=_app_ctx_stack.__ident_func__)
 
 
 @app.route("/")
@@ -19,10 +21,13 @@ def main():
 
 @app.route("/records/")
 def show_records():
-    db = SessionLocal()
-    records = db.query(models.Record).all()
-    db.close()
+    records = app.session.query(models.Record).all()
     return jsonify([record.to_dict() for record in records])
+
+
+@app.teardown_appcontext
+def remove_session(*args, **kwargs):
+    app.session.remove()
 
 
 if __name__ == "__main__":
