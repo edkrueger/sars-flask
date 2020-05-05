f1ff2fa3115934708a7e9e7536e1a44465ad77fe
sethkaufee <seth.s.kaufman@gmail.com>
2020-05-05 18:50:55 -0500

    some updates i made

--- app/main.py
+++ app/main.py
@@ -1,28 +1,24 @@
-from flask import Flask, _app_ctx_stack, jsonify, url_for
+from flask import Flask, jsonify, url_for
 from flask_cors import CORS
-from sqlalchemy.orm import scoped_session
 
-from . import models
-from .database import SessionLocal, engine
-
-models.Base.metadata.create_all(bind=engine)
+from app import models
+from app.database import db, init_db
 
 app = Flask(__name__)
 CORS(app)
-app.session = scoped_session(SessionLocal, scopefunc=_app_ctx_stack.__ident_func__)
+init_db(app)
 
 
 @app.route("/")
 def main():
-    return f"See the data at {url_for('show_records')}"
+    href = url_for("show_records")
+    return f'See the data at <a href="{href}">{href}</a>'
 
 
 @app.route("/records/")
 def show_records():
-    records = app.session.query(models.Record).all()
+
+    # records = app.db.query(models.Record).all()  # use db attribute
+    # records = models.Record.query.all()  # use query_property
+    records = db.query(models.Record).all()  # or use proxy
     return jsonify([record.to_dict() for record in records])
-
-
-@app.teardown_appcontext
-def remove_session(*args, **kwargs):
-    app.session.remove()
